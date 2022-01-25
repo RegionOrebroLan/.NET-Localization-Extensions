6aff8e287b6f44dbe7824a07f4d68c3f697849f0
HansKindberg <hans.kindberg@gmail.com>
2022-01-25 16:49:23 +0100

    Obsolete with error

--- Source/Project/Logging/Extensions/LoggerExtension.cs
+++ Source/Project/Logging/Extensions/LoggerExtension.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace RegionOrebroLan.Localization.Logging.Extensions
@@ -8,43 +8,43 @@ namespace RegionOrebroLan.Localization.Logging.Extensions
 	{
 		#region Methods
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogCriticalIfEnabled(this ILogger logger, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogCriticalIfEnabled(logger, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogCriticalIfEnabled(this ILogger logger, Exception exception, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogCriticalIfEnabled(logger, exception, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogDebugIfEnabled(this ILogger logger, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogDebugIfEnabled(logger, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogErrorIfEnabled(this ILogger logger, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogErrorIfEnabled(logger, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogErrorIfEnabled(this ILogger logger, Exception exception, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogErrorIfEnabled(logger, exception, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogInformationIfEnabled(this ILogger logger, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogInformationIfEnabled(logger, message, arguments);
 		}
 
-		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", false)]
+		[Obsolete("Use logging-extensions from the NuGet-package \"RegionOrebroLan.Logging\" instead.", true)]
 		public static void LogWarningIfEnabled(this ILogger logger, string message, params object[] arguments)
 		{
 			RegionOrebroLan.Logging.Extensions.LoggerExtension.LogWarningIfEnabled(logger, message, arguments);

76adf8254a20b72e863fd59dce7003f0f8b0768a
HansKindberg <hans.kindberg@gmail.com>
2023-06-01 17:05:34 +0200

    Samples => net 7.0

--- Samples/Resx-Localization/Program.cs
+++ Samples/Resx-Localization/Program.cs
@@ -1,22 +1,68 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using Application.DependencyInjection.Extensions;
+using Application.Localization.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Shared.DependencyInjection.Extensions;
+using Shared.Globalization;
+using Shared.Globalization.Configuration;
+using Shared.Web.Mvc.Configuration;
+using Shared.Web.Mvc.Filters;
+using Shared.Web.Routing;
 
-namespace Application
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ICultureFactory, CultureFactory>();
+builder.Services.AddSingleton(builder.Configuration.GetSection("CustomCultures").Get<IDictionary<string, CustomCultureOptions>>());
+
+builder.Services.Configure<LocalizationOptions>(builder.Configuration.GetSection("Localization"));
+builder.Services.AddLocalization();
+
+builder.Services.ConfigureRequestLocalization(builder.Configuration);
+
+builder.Services.Configure<RouteOptions>(routeOptions =>
 {
-	public static class Program
+	routeOptions.ConstraintMap.Add(RouteKeys.Culture, typeof(CultureRouteConstraint));
+	routeOptions.ConstraintMap.Add(RouteKeys.UiCulture, typeof(UiCultureRouteConstraint));
+});
+
+builder.Services.AddControllersWithViews(mvcOptions =>
+		{
+			mvcOptions.Filters.Add(typeof(LocalizationFilter));
+			mvcOptions.Filters.Add(new MiddlewareFilterAttribute(typeof(RequestLocalizationMiddlewareConfigurator)));
+		}
+	)
+	.AddDataAnnotationsLocalization(options =>
 	{
-		#region Methods
+		// This is needed only because we have our shared MVC-library. The name of that assembly is SharedMvc. If we did not have the code-snippet below the localizer would not be created correctly. It would have an incorrect path/resource-base-name.
+		options.DataAnnotationLocalizerProvider = (type, localizerFactory) => type.Assembly.Equals(typeof(StringLocalizerFactoryExtension).Assembly) ? localizerFactory.Create(type.FullName) : localizerFactory.Create(type);
+	})
+	.AddViewLocalization();
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-		{
-			return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
-		}
+builder.Services.ConfigureExample(builder.Configuration);
 
-		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
-		}
+var app = builder.Build();
 
-		#endregion
-	}
-}
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseRequestLocalization();
+
+var certificateControllerParameter = new { controller = "Certificate" };
+var notCertificateControllerConstraints = new { controller = "((?!Certificate).)*" }; // Another expression could be: "Form|Home|Information|Localization"
+
+app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute("Default-Localized", "{culture:culture}/{ui-culture:ui-culture}/{controller=Home}/{action=Index}/{id?}", null, notCertificateControllerConstraints);
+app.MapControllerRoute("Default-Localized-Culture", "{culture:culture}/{controller=Home}/{action=Index}/{id?}", null, notCertificateControllerConstraints);
+app.MapControllerRoute("Default-Localized-UI-Culture", "{ui-culture:ui-culture}/{controller=Home}/{action=Index}/{id?}", null, notCertificateControllerConstraints);
+
+app.MapControllerRoute("Certificate-Localized", "Certificate/{culture:culture}/{ui-culture:ui-culture}/{action=Index}/{id?}", certificateControllerParameter, certificateControllerParameter);
+app.MapControllerRoute("Certificate-Localized-Culture", "Certificate/{culture:culture}/{action=Index}/{id?}", certificateControllerParameter, certificateControllerParameter);
+app.MapControllerRoute("Certificate-Localized-UI-Culture", "Certificate/{ui-culture:ui-culture}/{action=Index}/{id?}", certificateControllerParameter, certificateControllerParameter);
+
+app.Run();
